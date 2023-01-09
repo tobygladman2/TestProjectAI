03b47047abd71a1becf248cfd7991fd3860a7483
tobygladman2 <91743957+tobygladman2@users.noreply.github.com>
2023-01-09 16:30:00 +0000

    Initial commit

---
+++ Source/TestProjectAI/TestProjectAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestProjectAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestProjectAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestProjectAIGameMode();
+};
+
+
+
